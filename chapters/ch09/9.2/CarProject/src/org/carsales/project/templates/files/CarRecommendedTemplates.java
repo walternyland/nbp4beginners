35a2c011e430334e0e6b433a92307308e700f254
walternyland@yahoo.com <>
2014-03-25 19:21:23 +0100

    privileged and recommended templates

--- chapters/ch09/9.2/CarProject/src/org/carsales/project/templates/files/CarRecommendedTemplates.java
+++ chapters/ch09/9.2/CarProject/src/org/carsales/project/templates/files/CarRecommendedTemplates.java
@@ -6,11 +6,10 @@ import org.netbeans.spi.project.ui.RecommendedTemplates;
         projectType = "org-carsales-project")
 public class CarRecommendedTemplates implements RecommendedTemplates {
     private static final String[] RECOMMENDED_NAMES = new String[]{
-        "simple-files",
+        "simple-files"
     };
     @Override
     public String[] getRecommendedTypes() {
-        System.out.println("recommended = " + RECOMMENDED_NAMES.toString());
         return RECOMMENDED_NAMES;
     }
 }
